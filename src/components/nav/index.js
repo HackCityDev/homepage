6e51b8ae2be2028526eb37cdee43b097919323a9
imransaadullah <imransaadullah@gmail.com>
2023-02-01 20:59:38 +0100

    Update 2 -- 01 - 02 - 2023: Modified the navbar and the social media handles

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -155,9 +155,9 @@ function Navbar() {
     return (
       <nav className="shadow-md w-full fixed top-0 left-0">
         <div className="nav-reduced">
-            <div className="md:flex bg-white py-4 items-center justify-between px-8 md:px-10 lg:px-18 xl:px-18 w-full">
-                <div className="md:flex bg-white py-4 items-center" onClick={() => navigate('/')}>
-                    <div className="flex bg-white py-4 items-center font-bold text-2xl cursor-pointer flex font-[Poppins] text-indigo-900">
+            <div className="md:flex bg-white py-2 items-center justify-between px-8 md:px-10 lg:px-18 xl:px-18 w-full">
+                <div className="md:flex bg-white py-2 items-center" onClick={() => navigate('/')}>
+                    <div className="flex bg-white py-2 items-center font-bold text-2xl cursor-pointer flex font-[Poppins] text-indigo-900">
                         <span href="#" className="text-white font-bold ">
                             <svg width="26" height="38" viewBox="0 0 26 38" fill="none" xmlns="http://www.w3.org/2000/svg" >
                                 <path d="M3.62791 0C1.62441 0 0 1.62852 0 3.63742V34.3626C0 36.3715 1.62441 38 3.62791 38C5.6314 38 7.25582 36.3715 7.25582 34.3626V24.8832C7.25582 23.5439 8.33875 22.4583 9.67442 22.4583H15.1163C16.4519 22.4583 17.5349 23.5439 17.5349 24.8832V34.3626C17.5349 36.3715 19.1593 38 21.1628 38C23.1663 38 24.7907 36.3715 24.7907 34.3626V20.0334C24.7907 17.3549 22.6251 15.1835 19.9535 15.1835H9.67442C8.33875 15.1835 7.25582 14.0978 7.25582 12.7585V3.63742C7.25582 1.62852 5.6314 0 3.62791 0Z" fill="url(#paint0_linear_815_250)"/>
@@ -180,7 +180,7 @@ function Navbar() {
                         </span>
                     </div>
                 </div>
-                <span className="text-3xl text-indigo-900 absolute right-8 top-16 cursor-pointer md:hidden" onClick={toggle}>
+                <span className="text-3xl text-indigo-900 absolute right-8 top-8 cursor-pointer md:hidden" onClick={toggle}>
                     <ion-icon name={isOpen? "close" : "menu-outline"}></ion-icon>
                 </span>
                 <ul className={`${isOpen ? 'top-32 opacity-100' : 'top-[-400px] md:opacity-100 opacity-0'} sm:block md:flex md:items-center font-[Poppins] md:py-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in font-medium`}>
